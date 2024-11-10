2f9f55d805e67c71744f50ecca9a9d87ff8b222e
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-11-10 16:58:57 +0900

    chore: 절대 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@pages': resolve(__dirname, 'src/pages'),
+      '@utils': resolve(__dirname, 'src/shared'),
+      '@components': resolve(__dirname, 'src/components')
+    }
+  }
+});
