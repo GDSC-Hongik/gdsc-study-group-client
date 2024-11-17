d09340c5549e6238c6ea19ded4bd13512e50a56c
Yujin Kim <yujii22001@gmail.com>
2024-11-17 17:03:30 +0900

    feat: 후기 상세페이지 랜더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import AppliedStudyItem from '../components/AppliedStudyItem';
+import BigStudyItem from '../components/BigStudyItem';
+import IntroduceItem from '../components/IntroduceItem';
+import SmallStudyItem from '../components/SmallStudyItem';
+import ModalReviewWrite from '../components/ModalReviewWrite';
 import './App.css';
+import React from 'react';
+import ModalReviewers from '../components/ModalReviewers';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ModalReviewers />
+    </>
+  );
 }
 
 export default App;
