e60d35352cec00e0c0edee70a386c412935234b3
KAITOKIDDA <twk031206@naver.com>
2025-01-22 09:00:16 +0900

    chore: 헤더와 스터디페이지 배치 임시 조정

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
 import './App.css';
 import React from 'react';
-import ModalReviewers from '../components/ModalReviewers';
 import { useState } from 'react';
 import StudyPage from '../src/components/StudyPage';
 import StudyApplyeeStatus from '../src/components/StudyApplyeeStatus';
 import StudyApplierStatus from '../src/components/StudyApplierStatus';
+//import ModalReviewers from '../components/ModalReviewers';
+import AppliedStudyItem from './components_yjin/AppliedStudyItem';
+import BigStudyItem from './components_yjin/BigStudyItem';
+import IntroduceItem from './components_yjin/IntroduceItem';
+import ModalApply from './components_yjin/ModalApply';
+import ModalReviewWrite from './components_yjin/ModalReviewWrite';
+import ModalReviewers from './components_yjin/ModalReviewers';
+import ModalStudyMake from './components_yjin/ModalStudyMake';
+import SmallStudyItem from './components_yjin/SmallStudyItem';
+import StudyList from './pages_yjin/StudyList';
+import NavBar from './components_yjin/NavBar';
+import GlobalNavBar from './components_hhhk/GlobalNavBar';
 
 function App() {
   const [view, setView] = useState('');
 
   return (
-    <div>
-      <h1>UI 테스트</h1>
+    <>
+      {/* 헤더(고정) */}
+      <NavBar />
 
-      <button onClick={() => setView('manager')}>스터디장 페이지</button>
-      <button onClick={() => setView('member')}>스터디원 페이지</button>
-      <button onClick={() => setView('applyeeStatus')}>지원현황 페이지</button>
-      <button onClick={() => setView('applierStatus')}>
-        지원상태확인 페이지
-      </button>
+      {/* 헤더 높이만큼 아래로 공간 확보 */}
+      <div style={{ marginTop: '286px' }}>
+        <h1>UI 테스트</h1>
+        <button onClick={() => setView('manager')}>스터디장 페이지</button>
+        <button onClick={() => setView('member')}>스터디원 페이지</button>
+        <button onClick={() => setView('applyeeStatus')}>
+          지원현황 페이지
+        </button>
+        <button onClick={() => setView('applierStatus')}>
+          지원상태확인 페이지
+        </button>
 
-      {view === 'manager' && <StudyPage userRole="manager" />}
-      {view === 'member' && <StudyPage userRole="member" />}
-      {view === 'applyeeStatus' && <StudyApplyeeStatus />}
-      {view === 'applierStatus' && <StudyApplierStatus />}
-    </div>
+        {/* 메인 콘텐츠 */}
+        {view === 'manager' && <StudyPage userRole="manager" />}
+        {view === 'member' && <StudyPage userRole="member" />}
+        {view === 'applyeeStatus' && <StudyApplyeeStatus />}
+        {view === 'applierStatus' && <StudyApplierStatus />}
+      </div>
+    </>
   );
 }
 
