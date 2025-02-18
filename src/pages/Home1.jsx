f3b3041361fb7d4667fd107a684b183e4097d5fe
Yujin Kim <yujii22001@gmail.com>
2025-02-18 16:44:12 +0900

    feat: 스터디 목록 불러오기 api

--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -1,19 +1,76 @@
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import NavBar from '../components_yjin/NavBar';
 import AppliedStudyItem from '../components_yjin/AppliedStudyItem';
 import StudyListArrow from '../assets/studylist-arrow.svg';
+import { useState } from 'react';
+import baseApi from '../api/baseApi';
+import { useEffect } from 'react';
+import ModalReviewCreate from '../components_yjin/ModalReviewCreate';
+
+const mocData = [
+  {
+    id: 1,
+    peopleNum: '1명 / 6명',
+    schedule: '화요일 19시',
+    type: 'GRAY',
+    buttonText: '지원 중...',
+    existContent: true
+  },
+  {
+    id: 2,
+    peopleNum: '제한 없음',
+    schedule: '화요일 19시, 수요일 19시',
+    type: 'BLUE',
+    buttonText: '지원하기',
+    existContent: true
+  },
+  {
+    id: 3,
+    peopleNum: '',
+    schedule: '',
+    type: 'BLUE',
+    buttonText: '개설하기',
+    existContent: false,
+    existTitle: false
+  }
+];
 
 const Home1 = () => {
   const nav = useNavigate();
   const moveStudyList = () => {
     nav('/study-list');
   };
+  const [studyCompo, setStudyCompo] = useState(mocData);
+
+  //개설하기 버튼을 눌러서 모달에서 스터디를 생성했을 때 홈페이지에 스터디가 추가된다.
+  async function fetchData() {
+    try {
+      const response = await baseApi.get('/studies'); // 스터디 전체 목록 가져와서 홈페이지에 보여주기
+      setStudyCompo(response.data);
+      alert('스터디 목록을 성공적으로 불러왔습니다.');
+    } catch (e) {
+      console.error(e);
+      alert('스터디 목록을 불러오는데 실패했습니다.');
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // 새 스터디 추가 함수
+  // const onStudyCreate = newStudy => {
+  //   setStudyCompo(prevStudies => [...prevStudies, newStudy]);
+  // };
+
   return (
     <>
       <NavBar />
       <Container className="content-wrapper">
+        <ModalReviewCreate />
         <Title>
           <div style={{ display: 'flex', gap: '10px' }}>
             <p1>스터디</p1>
@@ -26,6 +83,20 @@ const Home1 = () => {
             <img src={StudyListArrow}></img>
           </PlusList>
         </Title>
+
+        <FlexStudy>
+          {studyCompo.map(studyItem => (
+            <AppliedStudyItem
+              key={studyItem.id}
+              peopleNum={studyItem.peopleNum}
+              schedule={studyItem.schedule}
+              type={studyItem.type}
+              buttonText={studyItem.buttonText}
+              existContent={studyItem.existContent}
+              existTitle={studyItem.existTitle}
+            />
+          ))}
+        </FlexStudy>
 
         <FlexStudy>
           <AppliedStudyItem
