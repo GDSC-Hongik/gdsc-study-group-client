ad1cc5b53f8171b804307b3c1d147758c3b08d51
Yujin Kim <yujii22001@gmail.com>
2025-02-20 16:30:56 +0900

    feat: 변수 데이터  정리, api 호출

--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -8,48 +8,95 @@ import StudyListArrow from '../assets/studylist-arrow.svg';
 import { useState } from 'react';
 import baseApi from '../api/baseApi';
 import { useEffect } from 'react';
-import ModalReviewCreate from '../components_yjin/ModalReviewCreate';
 
 const mocData = [
   {
     id: 1,
-    peopleNum: '1명 / 6명',
-    schedule: '화요일 19시',
-    type: 'GRAY',
-    buttonText: '지원 중...',
-    existContent: true
+    name: '이펙티브 타입스크립트 스터디1',
+    description: '스터디 설명',
+    requirement: '요구조건',
+    question: '질문이 있어요',
+    maxParticipants: 6,
+    curriculums: [
+      {
+        week: 0,
+        subject: 'string'
+      }
+    ],
+    days: [
+      {
+        day: '화요일',
+        startTime: {
+          hour: 19,
+          minute: 0,
+          second: 0,
+          nano: 0
+        }
+      }
+    ],
+    studyStatus: 'OFFLINE',
+    status: 'PARTICIPANTS'
   },
   {
-    id: 2,
-    peopleNum: '제한 없음',
-    schedule: '화요일 19시, 수요일 19시',
-    type: 'BLUE',
-    buttonText: '지원하기',
-    existContent: true
-  },
-  {
-    id: 3,
-    peopleNum: '',
-    schedule: '',
-    type: 'BLUE',
-    buttonText: '개설하기',
-    existContent: false,
-    existTitle: false
+    id: 1,
+    name: '이펙티브 타입스크립트 스터디2',
+    description: '스터디 설명2',
+    requirement: '요구조건2',
+    question: '질문이 있어요2',
+    maxParticipants: 6,
+    curriculums: [
+      {
+        week: 0,
+        subject: 'string'
+      }
+    ],
+    days: [
+      {
+        day: '화요일',
+        startTime: {
+          hour: 19,
+          minute: 0,
+          second: 0,
+          nano: 0
+        }
+      }
+    ],
+    studyStatus: 'ONLINE',
+    status: 'CANCELED'
   }
 ];
+
+const convertMocData = apiData => {
+  return apiData.map(study => ({
+    id: study.id,
+    name: study.name,
+    description: study.description,
+    maxParticipants:
+      study.maxParticipants === 0
+        ? '제한 없음'
+        : `0명/${study.maxParticipants}명`,
+    schedule:
+      study.studyStatus === 'ONLINE'
+        ? '온라인'
+        : study.days?.length > 0
+          ? study.days.map(d => `${d.day} ${d.startTime.hour}시`).join(',')
+          : '일정 없음'
+  }));
+};
 
 const Home1 = () => {
   const nav = useNavigate();
   const moveStudyList = () => {
     nav('/study-list');
   };
-  const [studyCompo, setStudyCompo] = useState(mocData);
+  const [studyList, setStudyList] = useState(mocData); // useState([]) 스터디 목록 상태
+  const [status, setStatus] = useState('participants');
 
   //개설하기 버튼을 눌러서 모달에서 스터디를 생성했을 때 홈페이지에 스터디가 추가된다.
-  async function fetchData() {
+  async function fetchStudyList() {
     try {
       const response = await baseApi.get('/studies'); // 스터디 전체 목록 가져와서 홈페이지에 보여주기
-      setStudyCompo(response.data);
+      setStudyList(convertMocData(response.data)); // 변환된 데이터
       alert('스터디 목록을 성공적으로 불러왔습니다.');
     } catch (e) {
       console.error(e);
@@ -58,13 +105,32 @@ const Home1 = () => {
   }
 
   useEffect(() => {
-    fetchData();
+    fetchStudyList();
   }, []);
 
-  // 새 스터디 추가 함수
-  // const onStudyCreate = newStudy => {
-  //   setStudyCompo(prevStudies => [...prevStudies, newStudy]);
-  // };
+  // 새로운 스터디 추가 함수 -> AppliedStudyItem에 props로 전달
+  const addStudy = newStudy => {
+    setStudyList(prevStudies => [...prevStudies, newStudy]);
+  };
+
+  // 스터디 모집(지원) 상태에 따른 필터링
+  // 1. 모집 중인 스터디가 없으면 '개설하기' 스터디 하나만
+  // 2. 모집 중인(지원 안한) 스터디가 있으면 '지원하기' 스터디 보여주기
+  // 3. 지원 중인 스터디가 있으면  '지원 중..' 스터디 보여주기
+  // 4. 지원 완료된 스터디가 있으면 '이동하기' 스터디 보여주기
+  async function fetchStudyParticipants() {
+    try {
+      const response = await baseApi.get(`/studies/${study_id}/participants`);
+      setStatus(response.data.status);
+      alert('스터디 지원 상태를 성공적으로 불러왔습니다.');
+    } catch (e) {
+      console.error(e);
+      alert('스터디 지원 상태를 불러오는데 실패했습니다.');
+    }
+  }
+  useEffect(() => {
+    fetchStudyParticipants();
+  }, []);
 
   return (
     <>
@@ -72,8 +138,8 @@ const Home1 = () => {
       <Container className="content-wrapper">
         <Title>
           <div style={{ display: 'flex', gap: '10px' }}>
-            <p1>스터디</p1>
-            <p2>현재 모집 중인 스터디예요.</p2>
+            <Ptag className="studyTitle">스터디</Ptag>
+            <Ptag className="studyDiscript">현재 모집 중인 스터디예요.</Ptag>
           </div>
           <PlusList>
             <Ptag onClick={moveStudyList} className="list">
@@ -84,46 +150,25 @@ const Home1 = () => {
         </Title>
 
         <FlexStudy>
-          {studyCompo.map(studyItem => (
+          {studyList.slice(-3).map(studyItem => (
             <AppliedStudyItem
               key={studyItem.id}
-              peopleNum={studyItem.peopleNum}
+              name={studyItem.name}
+              description={studyItem.description}
+              maxParticipants={studyItem.maxParticipants}
               schedule={studyItem.schedule}
-              type={studyItem.type}
-              buttonText={studyItem.buttonText}
-              existContent={studyItem.existContent}
-              existTitle={studyItem.existTitle}
+              status={studyItem.status}
+              addStudy={addStudy}
             />
           ))}
         </FlexStudy>
 
-        <FlexStudy>
-          <AppliedStudyItem
-            peopleNum={'1명 / 6명'}
-            schedule={'화요일 19시'}
-            type={'GRAY'}
-            buttonText={'지원 중...'}
-            existContent={true}
-          />
-          <AppliedStudyItem
-            peopleNum={'제한 없음'}
-            schedule={'화요일 19시, 수요일 19시'}
-            type={'BLUE'}
-            buttonText={'지원하기'}
-            existContent={true}
-          />
-          <AppliedStudyItem
-            peopleNum={''}
-            schedule={''}
-            type={'BLUE'}
-            buttonText={'개설하기'}
-            existTitle={false}
-          />
-        </FlexStudy>
         <Title>
           <div style={{ display: 'flex', gap: '10px' }}>
-            <p1>활동 히스토리</p1>
-            <p2>다른 스터디들의 후기를 확인해요.</p2>
+            <Ptag className="studyTitle">활동 히스토리</Ptag>
+            <Ptag className="studyDiscript">
+              다른 스터디들의 후기를 확인해요.
+            </Ptag>
           </div>
         </Title>
         <Activity></Activity>
@@ -160,13 +205,26 @@ const Container = styled.div`
 const Ptag = styled.p`
   font-style: normal;
   line-height: normal;
-  color: #368ff7;
   &.list {
     font-size: 16px;
     font-weight: 600;
     letter-spacing: -0.4px;
+    color: #368ff7;
 
     cursor: pointer;
+  }
+  &.studyTitle {
+    font-size: 24px;
+    letter-spacing: -0.6px;
+  }
+  &.studyDiscript {
+    color: #6b6b6b;
+    font-size: 16px;
+    letter-spacing: -0.4px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end; /* div 높이 크기 내에서 맨 아래 붙게 위치하도록 한다. */
   }
 `;
 
@@ -176,19 +234,6 @@ const Title = styled.p`
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-  p1 {
-    font-size: 24px;
-    letter-spacing: -0.6px;
-  }
-  p2 {
-    color: #6b6b6b;
-    font-size: 16px;
-    letter-spacing: -0.4px;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end; /* div 높이 크기 내에서 맨 아래 붙게 위치하도록 한다. */
-  }
 
   display: flex;
   align-items: center;
