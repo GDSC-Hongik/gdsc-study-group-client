85972a6d71fe4a2ecae05b2e8419160e8314e489
Yujin Kim <yujii22001@gmail.com>
2025-02-20 20:26:34 +0900

    feat: mockData 분리

---
+++ data/studyData.js
@@ -0,0 +1,56 @@
+export const mocData = [
+  {
+    studyId: 1,
+    name: '이펙티브 타입스크립트 스터디1',
+    description: '스터디 설명',
+    requirement: '요구조건',
+    question: '질문이 있어요',
+    maxParticipants: 6,
+    curriculums: [
+      {
+        week: 0,
+        subject: 'string'
+      }
+    ],
+    days: [
+      {
+        day: '화요일',
+        startTime: {
+          hour: 19,
+          minute: 0,
+          second: 0,
+          nano: 0
+        }
+      }
+    ],
+    studyStatus: 'OFFLINE',
+    status: 'PARTICIPANTS'
+  },
+  {
+    studyId: 1,
+    name: '이펙티브 타입스크립트 스터디2',
+    description: '스터디 설명2',
+    requirement: '요구조건2',
+    question: '질문이 있어요2',
+    maxParticipants: 6,
+    curriculums: [
+      {
+        week: 0,
+        subject: 'string'
+      }
+    ],
+    days: [
+      {
+        day: '화요일',
+        startTime: {
+          hour: 19,
+          minute: 0,
+          second: 0,
+          nano: 0
+        }
+      }
+    ],
+    studyStatus: 'ONLINE',
+    status: 'CANCELED'
+  }
+];
